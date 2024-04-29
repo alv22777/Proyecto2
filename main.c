c795934c7b33d908763438c0da774932a602a7d8
alv22777 <alv22777@uvg.edu.gt>
2024-04-29 16:55:13 -0600

    Initial commit

---
+++ main.c
@@ -0,0 +1,18 @@
+/*
+ * Proyecto2.c
+ *
+ * Created: 4/29/2024 4:54:01 PM
+ * Author : gaadd
+ */ 
+
+#include <avr/io.h>
+
+
+int main(void)
+{
+    /* Replace with your application code */
+    while (1) 
+    {
+    }
+}
+
